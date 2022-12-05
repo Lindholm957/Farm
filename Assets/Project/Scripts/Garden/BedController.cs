570dc6450074d39be0f317e0075e1ef411d2ba9d
Nikita <yan957@gmail.com>
2022-12-05 17:56:40 +0300

    Main menu added

--- Assets/Project/Scripts/Garden/BedController.cs
+++ Assets/Project/Scripts/Garden/BedController.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Project.Scripts.Garden
 {
-    public class BedController : MonoBehaviour
+    public class BedController : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField] private MeshRenderer renderer;
 
+        public enum BedState
+        {
+            Empty,
+            Busy,
+            Ready
+        }
+
         public Vector3 Size => renderer.bounds.size;
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            ShowBedActivities();
+        }
+
+        private void ShowBedActivities()
+        {
+            
+        }
     }
 }
